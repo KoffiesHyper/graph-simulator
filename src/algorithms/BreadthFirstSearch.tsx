2c2f8327f1281e4e35bf73146d304a29817b83fb
Christian Slier <koffieshyper@Christians-MacBook-Air.local>
2023-01-12 21:03:18 +0200

    update

--- src/algorithms/BreadthFirstSearch.tsx
+++ src/algorithms/BreadthFirstSearch.tsx
@@ -1,7 +1,33 @@
+import { startTrackValue } from "@testing-library/user-event/dist/types/document/trackValue";
 import { NodeType } from "../features/graph/graphSlice";
 
-const applyBreadthFirstSearch = (nodes: NodeType[]) => {
-    
+interface BFS_Node extends NodeType {
+    visited: boolean,
+    prev: BFS_Node
+}
+
+let queue: BFS_Node[] = [];
+
+const applyBreadthFirstSearch = (nodes: NodeType[], start: NodeType, finish: NodeType) => {
+    let graph: BFS_Node[] = nodes.map(node => { return { ...node, visited: false, prev: {} as BFS_Node } });
+
+    let startNode = graph.find(node => node.label === start.label)!;
+
+    queue.push(startNode);
+
+    while(queue.length > 0){
+        for (let i = 0; i < queue.length; i++) {
+            for (let j = 0; j < queue[i].neighbours.length; j++) {
+                if(!isVisited(queue[i].neighbours[j])){
+                    setPrev
+                }
+            }
+        }
+    }
+}
+
+const isVisited = (node: NodeType) => {
+
 }
 
 export default applyBreadthFirstSearch;
