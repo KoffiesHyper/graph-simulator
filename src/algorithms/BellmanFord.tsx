b2faf8156a925d719633669a7691dec269af77d3
Christian Slier <koffieshyper@Christians-MacBook-Air.local>
2023-01-16 20:05:29 +0200

    update

--- src/algorithms/BellmanFord.tsx
+++ src/algorithms/BellmanFord.tsx
@@ -6,10 +6,25 @@ interface BF_Node extends NodeType {
 }
 
 let graph: BF_Node[] = [];
+
 const applyBellmanFord = (startNode: NodeType, nodes: NodeType[], edges: EdgeType[]) => {
     graph = nodes.map(node => { return { ...node, prev: undefined, dist: 0 } })
 
 
+}
+
+const orderEdges = (startNode: NodeType, nodes: NodeType[], edges: EdgeType[]) => {
+    let queue: NodeType[] = [];
+    queue.push(startNode);
+
+    while (queue.length > 0) {
+        for (let i = 0; i < queue.length; i++) {
+            for (let i = 0; i < array.length; i++) {
+                const element = array[i];
+                
+            }            
+        }
+    }
 }
 
 export default applyBellmanFord;
